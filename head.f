194fd661b98e04c8935375a40a049d2d8aa576f3
HugoPfoertner <117978741+HugoPfoertner@users.noreply.github.com>
2022-11-14 23:25:08 +0100

    Add files via upload
    Include license information

--- head.f
+++ head.f
@@ -1,4 +1,17 @@
 C Find strictly convex polygons on square grid of minimum area
-C Author: Hugo Pfoertner, 2018
+C Author: Hugo Pfoertner, 2018, 2022
+C Copyright 2022 Hugo Pfoertner
+C
+C   Licensed under the Apache License, Version 2.0 (the "License");
+C   you may not use this file except in compliance with the License.
+C   You may obtain a copy of the License at
+C
+C       http://www.apache.org/licenses/LICENSE-2.0
+C
+C   Unless required by applicable law or agreed to in writing, software
+C   distributed under the License is distributed on an "AS IS" BASIS,
+C   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+C   See the License for the specific language governing permissions and
+C   limitations under the License.
       implicit integer (A-Z)
       parameter (vlarge=2147483647)
